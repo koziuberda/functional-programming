36c66c19afdd7cff9cc76c243a41c8480dadad5b
skoziuberda <severyn.koziuberda@dataart.com>
2022-10-07 16:24:49 +0300

    Solve tasks in C#

---
+++ lab1/CSharp/Lab1/ListFunctions.cs
@@ -0,0 +1,34 @@
+﻿namespace Lab1;
+
+public static class ListFunctions
+{
+    public static IEnumerable<T> Singleton<T>(T element)
+    {
+        return new List<T> { element };
+    }
+
+    public static bool Null<T>(IEnumerable<T> list)
+    {
+        using var enumerator = list.GetEnumerator();
+
+        return !enumerator.MoveNext();
+    }
+
+    public static IEnumerable<T> Snoc<T>(IEnumerable<T> source, T newItem)
+    {
+        var newList = new List<T>(source) { newItem };
+        return newList;
+    }
+
+    public static int Lenght<T>(IEnumerable<T> source)
+    {
+        using var enumerator = source.GetEnumerator();
+        var counter = 0;
+        while (enumerator.MoveNext())
+        {
+            counter++;
+        }
+
+        return counter;
+    }
+}
---
+++ lab1/CSharp/Lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿using static Lab1.ListFunctions;
+
+var firstList = Singleton(5);
+
+Console.WriteLine("First list contains: ");
+foreach (var element in firstList)
+{
+    Console.WriteLine(element);
+}
+
+var isFirstListNull = Null(firstList);
+Console.WriteLine($"First list is empty: {isFirstListNull}");
+
+var secondList = Snoc(firstList, 6);
+
+Console.WriteLine("Second list contains: ");
+foreach (var element in secondList)
+{
+    Console.WriteLine(element);
+}
+
+var secondListLength = Lenght(secondList);
+Console.WriteLine($"Second list length: {secondListLength}");
+
